8f695e3a9c56d813e9e123976052bc9ca2356f4b
J Diaz <j.e.diaz@icloud.com>
2021-09-07 13:28:42 -0700

    set up files

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
+import "bootswatch/dist/slate/bootstrap.min.css";
+import Nav from "./components/Nav";
+
 
 function App() {
   return (
     <div className="App">
+      <Nav />
       <h1> My React Portfolio</h1>
     </div>
   );
