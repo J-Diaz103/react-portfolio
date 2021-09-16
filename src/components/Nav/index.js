5815047970c68dff42039f630f76820f54809717
J Diaz <j.e.diaz@icloud.com>
2021-09-15 22:25:44 -0700

    working on nav

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,10 +1,11 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 function Nav() {
     return (
         <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
             <div className="container-fluid">
-                <a className="navbar-brand" href="/" >Jesus Diaz</a>
+                <Link className="navbar-brand" href="/" >Jesus Diaz</Link>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarColor03" aria-controls="navbarColor03" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
@@ -12,16 +13,16 @@ function Nav() {
                 <div className="collapse navbar-collapse" id="navbarColor03">
                     <ul className="navbar-nav me-auto">
                         <li className="nav-item">
-                            <a className="nav-link " href="/aboutme" >About Me</a>
+                            <Link className="nav-link " to="/aboutme">About Me</Link>
                         </li>
                         <li classNames="nav-item">
-                            <a className="nav-link" href="/portfolio" >Portfolio</a>
+                            <Link className="nav-link" to="/portfolio" >Portfolio</Link>
                         </li>
                         <li className="nav-item">
-                            <a className="nav-link" href="/resume" >Resume</a>
+                            <Link className="nav-link" to="/resume" >Resume</Link>
                         </li>
                         <li className="nav-item">
-                            <a className="nav-link" href="/contact" >Contact</a>
+                            <Link className="nav-link" to="/contact" >Contact</Link>
                         </li>
                     </ul>
                 </div>
